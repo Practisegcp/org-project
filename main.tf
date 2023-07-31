dcb7ed4a43b46d5ef1b6af623e40b27906692539
root <root@kr-jenkins-cicd-vm.asia-east1-a.c.comman-host-2707.internal>
2023-07-31 13:49:04 +0000

    pp

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_project" "Service-frontend-0-0" {
-  name       = "Service-frontend-0-0"
-  project_id = "Serivce-frontend-0-0-0"
+resource "google_project" "service-frontend-0-0" {
+  name       = "service-frontend-0-0"
+  project_id = "serivce-frontend-0-0-0"
   org_id     = "267793989692"
 }
