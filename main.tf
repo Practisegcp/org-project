45d7d301a9e943fb44d2e3644b52922fecb8c20b
root <root@kr-jenkins-cicd-vm.asia-east1-a.c.comman-host-2707.internal>
2023-07-31 13:43:36 +0000

    p

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "google_project" "Service-frontend-0-0" {
+  name       = "Service-frontend-0-0"
+  project_id = "Serivce-frontend-0-0-0"
+  org_id     = "267793989692"
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.75.0"
+    }
+  }
+}
+provider "google" {
+    project = "comman-host-2707"
+  # Configuration options
+}
+
